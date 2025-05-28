631312737409a2a5a181216dda40e4e3c6bce41d
Alberto Gonzalez Rodriguez <josegonz@redhat.com>
2025-05-28 22:45:50 +0200

    Update setup-host1.sh

--- setup-automation/setup-host1.sh
+++ setup-automation/setup-host1.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-
+echo "[WebService]" > /etc/cockpit/cockpit.conf
+echo "https://cockpit-$(hostname -f|cut -d"-" -f2).apps.$(grep search /etc/resolv.conf| grep -o '[^ ]*$')" >> /etc/cockpit/cockpit.conf
+echo "AllowUnencrypted = true" >> /etc/cockpit/cockpit.conf
 systemctl enable --now cockpit.socket
